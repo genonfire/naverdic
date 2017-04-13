9dae16d0147d545ce9cb8d1515ecba400607bff7
KJ Kim <genonfire@gmail.com>
2017-04-13 16:55:09 +0900

    multiple audios in one page are now playable

--- popup.js
+++ popup.js
@@ -1,4 +1,7 @@
+var noAudios;
+
 function searchWord(e) {
+  noAudios = 0;
   var word = $('#dic').val();
   var queryURL = "http://endic.naver.com/searchAssistDict.nhn?query=" + word;
   
@@ -9,9 +12,14 @@ function searchWord(e) {
       success: function(data) {
           manipulated = parseAndManipulate(data);
           $('#content').html(manipulated);
-          play = document.getElementById("playaudio");
-          if (play)
-            play.addEventListener("click", playAudio);
+          for (var i = 0; i < noAudios; i++) {
+            play = document.getElementById("playaudio" + i);
+            if (play)
+              play.id = i;
+              play.addEventListener("click", function(e){
+                document.getElementById('proaudio' + this.id).play();
+              }, false);
+          }
       }
   });
 }
@@ -23,39 +31,40 @@ function onKeyPress(e) {
   }
 }
 
-function playAudio(e) {
-  document.getElementById('proaudio').play();
-}
-
 function parseAndManipulate(datain) {
   var data = datain.replace(/<a href="/gi, '<a href="http://endic.naver.com');
-  var audiostring = '';
-  var audiohead = data.indexOf('playlist="');
-  if (audiohead != -1) {
-    var audiotail = data.indexOf(' class=', audiohead + 10)
-    var audiosource = data.substring(audiohead + 10, audiotail);
+  while(true) {
+    var audiostring = '';
+    var audiohead = data.indexOf('playlist="');
+    if (audiohead != -1) {
+      var audiotail = data.indexOf(' class=', audiohead + 10)
+      var audiosource = data.substring(audiohead + 10, audiotail);
 
-    var playhead = data.indexOf('<img class="play"');
-    if (playhead != -1) {
-      var playtail = data.indexOf('" alt="', audiohead + 23);
-      var playsource = data.substring(playhead + 23, playtail);
+      var playhead = data.indexOf('<img class="play"');
+      if (playhead != -1) {
+        var playtail = data.indexOf('" alt="', audiohead + 23);
+        var playsource = data.substring(playhead + 23, playtail);
 
-      audiostring = '<audio src="' + audiosource + '" id="proaudio"></audio> <input type="image" id="playaudio" src="' + playsource + '">';
+        audiostring = '<audio src="' + audiosource + '" id="proaudio' + noAudios + '"></audio> <input type="image" id="playaudio' + noAudios + '" src="' + playsource + '">';
+      }
+      else {
+        audiostring = '<audio src="' + audiosource + '" id="proaudio' + noAudios + '"></audio> <input type="button" id="playaudio' + noAudios + '" value="play">';
+      }
+    } else {
+      return data;
     }
-    else {
-      audiostring = '<audio src="' + audiosource + '" id="proaudio"></audio> <input type="button" id="playaudio" value="play">';
-    }
+
+    var trashhead = data.indexOf('<a id="pron_en"');
+    var trashtail = data.indexOf('</a>', trashhead);
+
+    var datapart1 = data.substring(0, trashhead);
+    var datapart2 = data.substring(trashtail + 4);
+
+    data = datapart1 + audiostring + datapart2;
+    noAudios++;
   }
 
-  var trashhead = data.indexOf('<a id="pron_en"');
-  var trashtail = data.indexOf('</a>', trashhead);
-
-  var datapart1 = data.substring(0, trashhead);
-  var datapart2 = data.substring(trashtail + 4);
-
-  var manipulated = datapart1 + audiostring + datapart2;
-
-  return manipulated;
+  return data;
 }
 
 function onLoad() {
